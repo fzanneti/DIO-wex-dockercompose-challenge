e5e9412a95de9e8aa0536b16baa4a09e802394bd
Fabio Zanneti da Silva <fzanneti@gmail.com>
2025-07-20 13:14:09 -0300

    Update

--- Bot/JarbasBot/obj/Debug/net8.0/JarbasBot.AssemblyInfo.cs
+++ Bot/JarbasBot/obj/Debug/net8.0/JarbasBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JarbasBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d60a296f2291637e04ea51ba753a3096a44055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a18b2b8e9ae8b1824b5335c368f76222b8546c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JarbasBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JarbasBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
